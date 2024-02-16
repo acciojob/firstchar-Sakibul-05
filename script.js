ab1c63862963256f231ae43754d3c9de6d3a0ef1
Sk Sakibul Islam <110473516+Sakibul-05@users.noreply.github.com>
2024-02-16 22:06:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstChar(text) {
   // your code here
+	if(text){
+		 return text.trim()[0];
+	}else{
+		return "";
+	}
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
+const text = prompt("Enter text:")
 alert(firstChar(text));
